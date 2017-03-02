c8f0cc146760ec3d1f6f2b54d2a855c05674033a
Preetham N <preetham.in.1997@gmail.com>
2017-03-02 01:45:25 -0800

    Events work with state machine

--- StateManager.js
+++ StateManager.js
@@ -1,6 +1,8 @@
 class StateManager {
-	constructor() {
+	constructor(_state) {
+		this.currentState = _state;
 		this.states = {};
+		this.states[_state.name] = _state;
 	}
 
 	addState(stateName) {
@@ -9,6 +11,11 @@ class StateManager {
 
 	getState(stateName) {
 		return this.states[stateName];
+	}
+
+	apply(event) {
+		event.run();
+		this.currentState = this.currentState[event.name];
 	}
 }
 
@@ -28,32 +35,35 @@ class State {
 }
 
 class Event {
-	constructor(_name) {
+	constructor(_name, _activity) {
 		this.name = _name;
-		this.activityQueue = [];
-	}
-
-	addActivity(activity) {
-		this.activityQueue.push(activity);
+		this.activity = _activity;
 	}
 
 	run() {
-		for(var i=0; i<this.activityQueue.length; i++) this.activityQueue[i].run();
+		this.activity.run();
 	}
 }
 
 class Activity {
-	constructor(_audio, _function) {
+	constructor(_audio, _function, _event) {
 		this.audio = _audio;
 		this.func = _function;
-		this.audio.addEventListener("ended", this.func);
+		if(this.audio) this.audio.addEventListener("ended", this.func);
 	}
 
 	run() {
-		this.audio.play();
+		if(this.audio) this.audio.play();
+		else this.func;
 	}
 }
 
-var event = new Event("test");
-event.addActivity(new Activity(document.getElementById('AUDIOWOOHOO'), function(){console.log('testing activity')}));
-event.run();
+
+var SM = new StateManager(new State("root"));
+SM.addState(new State("second"));
+SM.getState("root").addChild("test", SM.getState("second"));
+
+var event1 = new Event("test", new Activity(document.getElementById('AUDIOWOOHOO'), function(){console.log('testing activity');}));
+var event2 = new Event("test2", new Activity(document.getElementById('AUDIORICH'), function(){console.log('testing null audio activity');}))
+
+SM.apply(event1);
